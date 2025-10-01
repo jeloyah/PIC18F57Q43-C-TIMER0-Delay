aee8845cd60e5953a1ce33f98a603448977a603f
edson <edson@edson-HP-ProDesk-400-G6-Desktop-Mini-PC>
2025-10-01 10:49:46 -0700

    Versión 1 29-sep-2025 - Se cambió el uso de registros del timer con la sintaxis de bit por bit. - Se modificó la forma en que se valida el fin del conteo del TIMER0.

---
+++ C-TIMER0-Delayms.X/main.c
@@ -0,0 +1,50 @@
+
+#include <xc.h>
+#include "main.h"
+#include "clock.h"
+#include "gpio.h"
+#include "timer0.h"
+#include "pps.h"
+
+void main(void) {
+    
+    /* System Clock Initialization */
+	CLOCK_Initialize();
+    
+    /* Config PortA */
+    PortA_Initialize();
+    
+    /* Config PortB */
+    PortB_Initialize();
+
+    /* Config PortC */
+    PortC_Initialize();
+    
+    /* Config PortD */
+    PortD_Initialize();
+    
+    /* Config PortE */
+    PortE_Initialize();
+    
+    /* Config PortF */
+    PortF_Initialize();
+    
+    /* Config TIMER0 */
+    TIMER0_Initialize();
+    
+    PPS_Initialize();
+      
+    /* infinite loop */
+    while(1)
+    {
+        LATF = 0x08;
+        TIMER0_Delay_ms(500);
+        
+        LATF = 0x00;
+        TIMER0_Delay_ms(30);
+        
+     } /* while */
+        
+    
+    return;
+} /* main*/
